8a9a07f3487a41e8cbe643a72fcf2272e0bc12e0
stayl <staylimmk@gmail.com>
2019-05-29 20:00:49 +0500

    Добавьте файлы проекта.

---
+++ task_ten/Program.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_ten
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.Write("Введите количество членов последовательности (не менее 2):   ");
+            Console.ResetColor();
+            int n = EnterAnInteger();
+            Row numSequence = new Row();
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine(@"
+Заполнить...
+1. рандомно.
+2. с клавиатуры.");
+            Console.ResetColor();
+            switch(MenuOption())
+            {
+                case 1:
+                    numSequence = new Row(n);
+                    break;
+                case 2:
+                    for (int i = 0; i < n; i++)
+                    {
+                        numSequence.Add(EnterANumber());
+                    }
+                    break;
+            }
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.WriteLine("\nПоследовательность");
+            Console.ResetColor();
+            PrintRow(numSequence);
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.WriteLine("\n\nПоследовательность {x[1] - x[n], x[2] - x[n], ..., x[n-1] - x[n]}");
+            Console.ResetColor();
+            Row newSequence = new Row();
+            for (int i = 0; i < n-1; i++)
+                newSequence.Add(numSequence[i] - numSequence[n - 1]);
+            PrintRow(newSequence);
+
+            Console.CursorVisible = false;
+            Console.ReadKey();
+        }
+
+        public static void PrintRow(Row row)
+        {
+            foreach (double item in row)
+                Console.Write(item + "\t");
+        }
+
+        public static int EnterAnInteger() //ввод
+        {
+            bool ok = false;
+            int number = 0;
+            do
+            {
+                try
+                {
+                    number = Convert.ToInt32(Console.ReadLine());
+                    ok = true;
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Введите целое число!");
+                }
+            } while (!ok);
+            return number;
+        }
+
+        public static double EnterANumber() //ввод
+        {
+            Console.Write("Введите число:   ");
+            bool ok = false;
+            double number = 0;
+            do
+            {
+                try
+                {
+                    number = Convert.ToDouble(Console.ReadLine());
+                    ok = true;
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Введите целое число!");
+                }
+            } while (!ok);
+            return number;
+        }
+
+        public static int MenuOption()
+        {
+            int option = 0;
+            bool alright = false;
+            do
+            {
+                Console.ForegroundColor = ConsoleColor.DarkYellow;
+                Console.Write("Пункт:    ");
+                Console.ResetColor();
+                try
+                {
+                    option = Convert.ToInt32(Console.ReadLine());
+                    if (option < 1 || option > 2) Console.WriteLine("Error!");
+                    else alright = true;
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Error!");
+                    alright = false;
+                }
+            } while (!alright);
+
+            return option;
+        }
+
+    }
+}
---
+++ task_ten/Row.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_ten
+{
+    public class Row : IEnumerable
+    {
+        static Random rand = new Random();
+        Node head;
+
+        public Row()
+        {
+            head = null;
+            Count = 0;
+        }
+
+        public Row(double element)
+        {
+            Count = 1;
+            head = new Node(element);
+        }
+
+        public Row(int capasity) // сучайное заполнение
+        {
+            Count = capasity;
+            int i = 0;
+            head = new Node(rand.Next(-100, 100) + rand.Next(-10, 10) / 10);
+            Node current = head;
+            while (i < capasity - 1)
+            {
+                current.next = new Node(rand.Next(-100, 100));
+                current = current.next;
+                i++;
+            }
+        }
+
+        public double this[int index]
+        {
+            get
+            {
+                double[] item = this.ToArray();
+                return item[index];
+            }
+            set
+            {
+                double[] item = this.ToArray();
+                item[index] = value;
+                this.Clear();
+                foreach (int t in item)
+                {
+                    this.Add(t);
+                }
+            }
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            Node current = head;
+            while (current != null)
+            {
+                yield return current.Value;
+                current = current.next;
+            }
+        }
+
+        public void Add(double element)
+        {
+            Node node = new Node(element);
+            if (head == null) head = node;
+            else
+            {
+                Node current = head;
+                while (current.next != null) current = current.next;
+                current.next = node;
+            }
+            Count++;
+        }
+
+        public void Clear()
+        {
+            head = null;
+            Count = 0;
+        }
+
+        public double[] ToArray()
+        {
+            Node current = head;
+            double[] array = new double[this.Count];
+            int i = 0;
+            while (current != null)
+            {
+                array[i] = current.Value;
+                i++;
+                current = current.next;
+            }
+            return array;
+        }
+
+        public int Count { get; private set; }
+    }
+
+    internal class Node
+    {
+        internal double item;
+        internal Node next;
+
+        public Node()
+        {
+            item = 0;
+            next = null;
+        }
+
+        public Node(double element)
+        {
+            item = element;
+            next = null;
+        }
+
+        public double Value
+        {
+            get { return item; }
+        }
+
+        public override string ToString()
+        {
+            return item.ToString();
+        }
+    }
+}
